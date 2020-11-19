65b7915750b7a23dff2a5dc52b1e95203ddab07c
Keiran Wilkinson <keiran@keiranwilkinson.com>
2020-11-19 12:42:57 +0000

    Eventbrite API now gets events

--- eventBriteAPI.js
+++ eventBriteAPI.js
@@ -1,18 +1,24 @@
-const fs = require('fs');
+console.log(getEvents());
 
-fs.readFile('eventbrite.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    var keys = JSON.parse(content);
-    console.log(keys.token)
+function getEvents() {
 
-    const eventbrite = require('eventbrite').default;
-    // Create configured Eventbrite SDK
-    const sdk = eventbrite({ token: keys.token });
+    const fs = require('fs');
 
-    // See: https://www.eventbrite.com/developer/v3/endpoints/users/#ebapi-get-users-id
-    sdk.request('/users/me').then(res => {
-console.log(res)    });
+    fs.readFile('eventbrite.json', (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        var keys = JSON.parse(content);
+        console.log(keys.token)
 
-});
+        const eventbrite = require('eventbrite').default;
+        // Create configured Eventbrite SDK
+        const sdk = eventbrite({ token: keys.token });
+
+        // See: https://www.eventbrite.com/developer/v3/endpoints/users/#ebapi-get-users-id
+        sdk.request('/organizations/464103861019/events/').then(res => {
+console.log(res.events)     });
+
+    });
+}
+
 
 
